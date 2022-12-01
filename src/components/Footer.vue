73eb62b6f002a9de74f3ba5a4ed5cc66055a902f
gargi kapadia <gargi.kapadia28@gmail.com>
2022-11-30 20:52:13 -0500

    worked on front page

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,9 +2,14 @@
 </script>
 
 <template>
-
+<p> <pre>Since 2022 | Founder by Gargi Kapadia |
+At Movies.CO our mission is to supply users with a variety of movie options that are easily assesible for 
+all your entertaniment needs.
+</pre></p>
 </template>
 
 <style scoped>
-
+p {
+    color: #213547;
+}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,9 +2,22 @@
 </script>
 
 <template>
-
+<div>
+<h1>Movies.CO</h1>
+<button class="button">Login</button>
+</div>
 </template>
 
 <style scoped>
+.button {
+    background-color: darkgray;
+    margin-bottom: 50px;
+}
+
+h1 {
+    font-size: 100px;
+    margin-bottom: 5px;
+    margin-top: 5px;
+}
 
 </style>
